84c996b188d322d982edf9403016fa2fa08a3722
jiangxing <1030486238@qq.com>
2021-09-02 17:50:49 +0800

    完成了首页导航栏navbar

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,26 @@
 <template>
-  <h2>
-    首页
-  </h2>
+  <div id="Home">
+    <nav-bar class="home-nav">
+      <div class="center" slot="center">购物车</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar";
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    NavBar
+  }
 }
 </script>
 
 <style scoped>
-
+  .home-nav {
+    background-color: var(--color-tint);
+  }
+  .center {
+    color: #f6f6f6;
+  }
 </style>
