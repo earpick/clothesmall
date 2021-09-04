4205764ce109d4d3a29429a3a0e2c1242643212f
jiangxing <1030486238@qq.com>
2021-09-04 16:15:27 +0800

    完成了推荐功能模块

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,12 +4,14 @@
       <div class="center" slot="center">购物车</div>
     </nav-bar>
     <home-swiper :swiperItemData="banner"/>
+    <recommend-view :recommendItemData="recommend"/>
   </div>
 </template>
 
 <script>
   import NavBar from "components/common/navbar/NavBar";
   import HomeSwiper from "views/home/childComps/HomeSwiper";
+  import RecommendView from "views/home/childComps/RecommendView";
 
   import {getHomeMultidata} from "network/home";
 
@@ -17,17 +19,20 @@
     name: "Home",
     components: {
       NavBar,
-      HomeSwiper
+      HomeSwiper,
+      RecommendView
     },
     created() {
       getHomeMultidata().then(result => {
         console.log(result)
         this.banner = result.data.data.banner.list;
+        this.recommend = result.data.data.recommend.list;
       })
     },
     data() {
       return {
-        banner: []
+        banner: [],
+        recommend: []
       }
     }
   }
